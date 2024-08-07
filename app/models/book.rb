d44ab25760ea0493a671bbbbdcd56b99494f3ac4
grez-lucas <lucasgrez3010@gmail.com>
2024-08-07 18:15:00 -0400

    [feat(books)] Add search functionality and pagination
    Also made index prettier using a table

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,18 @@ class Book < ApplicationRecord
   belongs_to :author
   has_many :reviews, dependent: :destroy
   has_many :sales, dependent: :destroy
+
+
+  # Search bar logic for words in description
+  scope :search_by_summary, ->(query) {
+    if query.present?
+      words = query.downcase.split(' ')
+      where(
+        words.map { |word| "summary ILIKE ?" }.join(' OR '),
+        *words.map { |word| "%#{word}%" }
+      )
+    else
+      all
+    end
+  }
 end
